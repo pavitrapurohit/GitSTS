7628ad6a81dafb8bd04242282a3b3ec78a730e90
papurohi <papurohi@DIN73003009.corp.capgemini.com>
2018-11-23 14:21:46 +0530

    My first commit

---
+++ GitImpl/src/main/java/org/cap/demo/MainClasss.java
@@ -0,0 +1,13 @@
+package org.cap.demo;
+
+public class MainClasss {
+
+	public static void main(String[] args) {
+		PrintHello();
+
+	}
+	public static void PrintHello() {
+		System.out.println("Hiii!!!!!!!!! Its afternoon..having headache!!");
+	}
+
+}
